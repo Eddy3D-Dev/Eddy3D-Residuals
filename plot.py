db143b39db79cbe2ca4d39ff6252509c472bc9d0
Patrick Kastner <patrick.kastner@gmail.com>
2020-07-14 15:19:33 -0400

    Initial commit

---
+++ plot.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pathlib
+import os
+
+w_dir = pathlib.Path.cwd()
+
+residual_files = []
+for root, dirs, files in os.walk(w_dir):
+    for file in files:
+        if file.endswith(".dat"):
+            residual_files.append(os.path.join(root, file))
+
+for file in residual_files:
+    data = pd.read_csv(file, skiprows=1, delimiter='\s+').iloc[:, 1:].shift(+1, axis=1).drop(["Time"], axis=1)
+    plot = data.plot(logy=True, figsize=(15, 5))
+    fig = plot.get_figure()
+    ax = plt.gca()
+    ax.legend(loc='upper right')
+    ax.set_xlabel("Iterations")
+    ax.set_ylabel("Residuals")
+    wind_dir = str(file).split('\\')[-5]
+    iteration = str(file).split('\\')[-2]
+    plt.savefig( wind_dir+ "_" + iteration + "_residuals.png", dpi=600)
